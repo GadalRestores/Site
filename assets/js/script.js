0599acccfbfbcafc84b6cd1f82e993e5cbecb0cf
GadalRestores <gadalrestores@gmail.com>
2026-07-03 02:22:21 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
-// ====================================
-// GADAL RESTORES
-// Main JavaScript File
-// ====================================
+// ============================
+// Gadal Restores
+// Premium Website
+// ============================
 
-console.log("Website Loaded Successfully");
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.style.background = "#050505";
+
+    }
+
+    else{
+
+        navbar.style.background = "rgba(5,5,5,.55)";
+
+    }
+
+});
