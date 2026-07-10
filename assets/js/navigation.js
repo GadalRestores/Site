1d8df547ea6ffa65ce2c8d2c655eaf310bc746fa
GadalRestores <gadalrestores@gmail.com>
2026-07-10 15:39:36 -0400

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,1 +1,25 @@
+/* ==========================================
+   GADAL RESTORES
+   Navigation
+========================================== */
 
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 40) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
+
+const menuButton = document.querySelector(".menu-button");
+const desktopNav = document.querySelector(".desktop-nav");
+
+menuButton.addEventListener("click", () => {
+
+    desktopNav.classList.toggle("mobile-open");
+
+});
